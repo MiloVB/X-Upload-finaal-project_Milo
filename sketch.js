d0148fc24a052d71695a86dc95a52d052b61e7c2
unknown <r1102612@student.thomasmore.be>
2026-05-23 20:19:33 +0200

    js async

--- sketch.js
+++ sketch.js
@@ -39,3 +39,104 @@ const brands = [
 
 
 ];
+
+const logo = document.getElementById("logo");
+const labelContainer =
+    document.getElementById("label-container");
+
+const scoreText =
+    document.getElementById("score");
+
+const bestScoreText =
+    document.getElementById("bestScore");
+
+function loadBrand(){
+
+    logo.src = brands[currentBrand].image;
+}
+
+async function createModel() {
+
+    const checkpointURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    recognizer = window.speechCommands.create(
+        "BROWSER_FFT",
+        undefined,
+        checkpointURL,
+        metadataURL
+    );
+
+    await recognizer.ensureModelLoaded();
+}
+
+
+async function init() {
+
+    if(isListening){
+        return;
+    }
+
+    try {
+
+        isListening = true;
+
+        labelContainer.innerHTML =
+            "⏳ Loading model...";
+
+        await createModel();
+
+        const classLabels =
+            recognizer.wordLabels();
+
+        labelContainer.innerHTML =
+            "🎧 Listening... Say the brand name!";
+
+        recognizer.listen(result => {
+
+            const scores = result.scores;
+
+            let highestScore = 0;
+            let predictedClass = "";
+
+            for (let i = 0; i < classLabels.length; i++) {
+
+                if(scores[i] > highestScore){
+
+                    highestScore = scores[i];
+                    predictedClass = classLabels[i];
+                }
+            }
+
+            console.log(
+                "Detected:",
+                predictedClass,
+                highestScore
+            );
+
+            if(highestScore > 0.75){
+
+                checkAnswer(predictedClass);
+            }
+
+        }, {
+
+            includeSpectrogram: true,
+            probabilityThreshold: 0.75,
+            invokeCallbackOnNoiseAndUnknown: true,
+            overlapFactor: 0.5
+
+        });
+
+    } catch(error){
+
+        console.error(error);
+
+        labelContainer.innerHTML =
+            "❌ Error: " + error.message;
+
+        alert(error.message);
+
+        isListening = false;
+    }
+}
